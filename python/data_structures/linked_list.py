a35278e2ab5086b28578417b0b63e195ea9f1a12
brianjtarte <brianjtarte@gmail.com>
2022-05-31 21:43:29 -0700

    partially got through code challenge 5, having issues with PyCharm

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,14 +1,28 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+    def __init__(self, head=None):
+        self.head = head
 
-    def __init__(self):
-        # initialization here
-        pass
+    def insert(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
 
-    def some_method(self):
-        # method body here
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def to_string(self):
         pass
 
 
