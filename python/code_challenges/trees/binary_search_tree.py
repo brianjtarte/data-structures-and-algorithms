8c5cb7528c87738f85af47550a421590e6cc3e4e
brianjtarte <brianjtarte@gmail.com>
2022-07-12 18:05:36 -0700

    finished challenge 15 and 16

--- python/code_challenges/trees/binary_search_tree.py
+++ python/code_challenges/trees/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from python.code_challenges.trees.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -7,9 +7,43 @@ class BinarySearchTree(BinaryTree):
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.root = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        node = Node(value)
+
+        if not self.root:
+            self.root = node
+            return
+
+        def traverse_tree(root, node):
+            if not root:
+                return
+
+            if node.value < root.value:
+                if root.left:
+                    traverse_tree(root.left, node)
+                else:
+                    root.left = node
+            else:
+                if root.right:
+                    traverse_tree(root.right, node)
+                else:
+                    root.right = node
+
+        traverse_tree(self.root, node)
+
+    def contains(self, value):
+
+        def traverse_tree(root):
+            if not root:
+                return False
+
+            elif root.value == value:
+                return True
+            elif value < root.value:
+                return traverse_tree(root.left)
+            else:
+                return traverse_tree(root.right)
+
+        return traverse_tree(self.root)
--- python/code_challenges/trees/binary_tree.py
+++ python/code_challenges/trees/binary_tree.py
@@ -4,13 +4,77 @@ class BinaryTree:
     """
 
     def __init__(self):
-        # initialization here
         pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def pre_order(self):
+
+        def traverse_tree(root):
+
+            values.append(root.value)
+
+            if root.left:
+                traverse_tree(root.left)
+
+            if root.right:
+                traverse_tree(root.right)
+
+        values = []
+
+        traverse_tree(self.root)
+
+        return values
+
+    def in_order(self):
+
+        def traverse_tree(root):
+
+            if root.left:
+                traverse_tree(root.left)
+            values.append(root.value)
+
+            if root.right:
+                traverse_tree(root.right)
+
+        values = []
+
+        traverse_tree(self.root)
+
+        return values
+
+    def post_order(self):
+
+        def traverse_tree(root):
+
+            if root.left:
+                traverse_tree(root.left)
+
+            if root.right:
+                traverse_tree(root.right)
+            values.append(root.value)
+
+        values = []
+
+        traverse_tree(self.root)
+
+        return values
+
+    def find_maximum_value(self):
+
+        def traverse_tree(root, max_value=0):
+            if root is None:
+                return max_value
+            if root.value > max_value:
+                max_value = root.value
+            return traverse_tree(root.left, max_value)
+            return traverse_tree(root.right, max_value)
+
+        return traverse_tree(self.root)
+
+
 
 
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
