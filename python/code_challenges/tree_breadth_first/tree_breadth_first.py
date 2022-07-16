5fcb87631f38109058f49cd299183fdcb0cc94f5
brianjtarte <brianjtarte@gmail.com>
2022-07-15 20:09:55 -0700

    Completed code and passed all tests

--- python/code_challenges/tree_breadth_first/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first/tree_breadth_first.py
@@ -1,7 +1,22 @@
 from python.code_challenges.trees.binary_tree import BinaryTree
-from python.code_challenges.stack_and_queue import queue
+from python.code_challenges.stack_and_queue.queue import Queue
 
 
-
-def breadth_first():
+def breadth_first(tree):
     tree_queue = Queue()
+
+    tree_list = []
+
+    if tree.root is None:
+        return tree_list
+
+    tree_queue.enqueue(tree.root)
+
+    while tree_queue.front:
+        front_node = tree_queue.dequeue()
+        tree_list.append(front_node.value)
+        if front_node.left:
+            tree_queue.enqueue(front_node.left)
+        if front_node.right:
+            tree_queue.enqueue(front_node.right)
+    return tree_list
