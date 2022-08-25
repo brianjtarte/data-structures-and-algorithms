64e3a1bd4dc4b029cb9ee3b70cfcd5179465815d
brianjtarte <brianjtarte@gmail.com>
2022-08-24 22:13:28 -0700

    completed challenge 31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,20 @@
-from data_structures.hashtable import Hashtable
+from python.data_structures.hashtable import Hashtable
+import string
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(words):
+    new_table = Hashtable()
+    newstring = words.translate(str.maketrans('', '', string.punctuation))
+    key_list = newstring.split()
+
+    for key in key_list:
+        newkey = key.lower()
+        if new_table.contains(newkey):
+
+            return newkey
+        else:
+            new_table.set(newkey, None)
+    return None
+
+
+
---
+++ python/docs/hashtable_repeated_word/README.md
@@ -0,0 +1,18 @@
+# Challenge Summary
+**Find the first repeated word in a book**
+
+## Collaborator: Brendon H.
+
+## Feature Tasks:
+ - Write a function called `repeated word` that finds the first word to occur more than once in a string
+   - Arguments: string
+   - Return: string
+
+## Whiteboard Process
+![repeated_word](hashtable-repeated-word.png)
+
+## Approach & Efficiency
+Space: O(n) because we use a for loop
+Time: O(1) because it runs one time
+## Solution
+pytest tests/code_challenges/test_hashable_repeated_word.py
